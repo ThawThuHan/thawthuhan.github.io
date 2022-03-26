ebea06d198a3ba4f967134ac38095d5906073edd
ThawThuHan <thawthuhan9@gmail.com>
2022-03-26 08:42:15 +0630

    change bmi style and add some function

--- bmi-calculator/js/index.js
+++ bmi-calculator/js/index.js
@@ -4,7 +4,10 @@ const heightSlide = document.querySelector('#height');
 const weightSlide = document.querySelector('#weight');
 const heightNumber = document.querySelector(".height");
 const weightNumber = document.querySelector(".weight");
-const calculateBtn = document.querySelector("#calculate")
+const calculateBtn = document.querySelector("#calculate");
+const under = document.querySelector('.under');
+const normal = document.querySelector('.normal');
+const over = document.querySelector('.over');
 
 let gender = "male", height, weight;
 
@@ -51,7 +54,55 @@ weightNumber.oninput = () => {
 
 calculateBtn.onclick = function () {
     const result = document.querySelector('.result');
+
     console.log();
     let bmi = (703 * weight) / (height ** 2);
-    result.firstElementChild.innerHTML = bmi.toFixed(1);
+    if (!isNaN(bmi)) {
+        result.firstElementChild.innerHTML = bmi.toFixed(1) + " BIM";
+    } else {
+        result.firstElementChild.innerHTML = "Error!";
+    }
+
+    if (gender == "male") {
+        console.log(gender);
+        if (bmi < 17.8) {
+            removeClass("under")
+        } else if (17.8 > bmi && bmi < 23.6) {
+            removeClass("normal");
+        } else {
+            removeClass("over");
+        }
+    } else {
+        if (bmi < 17.8) {
+            removeClass("under");
+        } else if (17.8 > bmi && bmi < 25.6) {
+            removeClass("normal");
+        } else {
+            removeClass("over");
+        }
+    }
+
+}
+
+function removeClass(info) {
+    switch (info) {
+        case "under": {
+            under.classList.add("result-active");
+            normal.classList.remove("result-active");
+            over.classList.remove("result-active");
+        }
+            break;
+        case "normal": {
+            under.classList.remove("result-active");
+            normal.classList.add("result-active");
+            over.classList.remove("result-active");
+        }
+            break;
+        case "over": {
+            under.classList.remove("result-active");
+            normal.classList.remove("result-active");
+            over.classList.add("result-active");
+        }
+            break;
+    }
 }
