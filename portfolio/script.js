06a3244c52803ea6a7d9ce26e46b626171adee3b
ThawThuHan <thawthuhan9@gmail.com>
2022-04-06 11:35:45 +0630

    Project Page Add

--- portfolio/script.js
+++ portfolio/script.js
@@ -1,16 +1,24 @@
 const menuItems = document.querySelector(".menu-items");
 const home = document.querySelector("#home");
 const about = document.querySelector("#about");
+const project = document.querySelector("#project");
 
 for (let i of menuItems.children) {
     i.addEventListener('click', (e) => {
         let element = e.target;
         let target = element.innerText.trim();
+        console.log(target);
         if (target == "Home") {
             home.classList.remove('d-none');
             about.classList.add('d-none');
-        } else if (target = "About") {
+            project.classList.add('d-none');
+        } else if (target == "About") {
             about.classList.remove('d-none');
+            home.classList.add('d-none');
+            project.classList.add('d-none');
+        } else if (target == "Project") {
+            project.classList.remove('d-none');
+            about.classList.add('d-none');
             home.classList.add('d-none');
         }
 
