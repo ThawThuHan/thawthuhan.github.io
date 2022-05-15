80cfb22188b6a34a8d3f541c134a1a4f222a53e0
ThawThuHan <thawthuhan9@gmail.com>
2022-05-15 08:21:46 +0630

    add portfolio menu-toggle

--- portfolio/script.js
+++ portfolio/script.js
@@ -2,6 +2,51 @@ const menuItems = document.querySelector(".menu-items");
 const home = document.querySelector("#home");
 const about = document.querySelector("#about");
 const project = document.querySelector("#project");
+const menuToggle = document.querySelector(".menu-toggle");
+const hanbugerMenu = document.querySelector(".hanbuger-menu")
+const menuliTag = document.querySelectorAll(".hanbuger-menu ul li");
+
+menuToggle.addEventListener("click", (event) => {
+    if (hanbugerMenu.classList.contains("showed")) {
+        hanbugerMenu.classList.remove("showed");
+        hanbugerMenu.style.opacity = "0";
+        menuliTag.forEach(e => {
+            e.classList.remove("menu-animation")
+        })
+    } else {
+        hanbugerMenu.classList.add("showed");
+        hanbugerMenu.style.opacity = "1";
+        menuliTag.forEach(e => {
+            e.classList.add("menu-animation")
+        })
+    }
+})
+
+menuliTag.forEach(e => {
+    e.addEventListener("click", (event) => {
+        let target = event.target.innerText;
+        if (hanbugerMenu.classList.contains("showed")) {
+            hanbugerMenu.classList.remove("showed");
+            hanbugerMenu.style.opacity = "0";
+            menuliTag.forEach(e => {
+                e.classList.remove("menu-animation")
+            })
+        }
+        if (target == "Home") {
+            home.classList.remove('d-none');
+            about.classList.add('d-none');
+            project.classList.add('d-none');
+        } else if (target == "About") {
+            about.classList.remove('d-none');
+            home.classList.add('d-none');
+            project.classList.add('d-none');
+        } else if (target == "Project") {
+            project.classList.remove('d-none');
+            about.classList.add('d-none');
+            home.classList.add('d-none');
+        }
+    })
+})
 
 for (let i of menuItems.children) {
     i.addEventListener('click', (e) => {
